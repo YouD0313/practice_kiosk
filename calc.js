820cbf5a5b982aa4aa99c30aeee791d516dd4c4b
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2022-11-14 17:47:51 +0900

    js money

--- calc.js
+++ calc.js
@@ -1,7 +1,8 @@
 let sum = 0;
-
-function calc(button) {
-	if (button) sum += parseInt(button.value);
-
+const spin = document.querySelectorAll('.spin');
+const button = document.querySelectorAll('.cartIcon');
+button.addEventListener('click', function () {
+	sum += parseInt(button.value) * parseInt(spin.value);
 	document.getElementById('sumtext').value = sum + '원';
-}
+	spin.value = 1;
+});
