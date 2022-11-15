95445365b9a958a266273a97eb7eda30a83da0b0
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2022-11-15 10:33:31 +0900

    javascript forEach ver1

--- calc.js
+++ calc.js
@@ -1,8 +1,28 @@
 let sum = 0;
 const spin = document.querySelectorAll('.spin');
 const button = document.querySelectorAll('.cartIcon');
-button.addEventListener('click', function () {
-	sum += parseInt(button.value) * parseInt(spin.value);
-	document.getElementById('sumtext').value = sum + '원';
-	spin.value = 1;
+
+// button.addEventListener('click', function () {
+// 	sum += parseInt(button.value) * parseInt(spin.value);
+// 	document.getElementById('sumtext').value = sum + '원';
+// 	spin.value = 1;
+// });
+
+button.forEach((cart, i) => {
+	cart.addEventListener('click', (event) => {
+		const target = event.target;
+		const link = target.dataset.index;
+		console.log('target', target);
+		console.log('link', link);
+		button.forEach(() => {
+			// if (cart == i) {
+			sum += parseInt(button[i].value) * parseInt(spin[i].value);
+			document.getElementById('sumtext').value = sum + '원';
+			spin[i].value = 1;
+			console.log('sum', sum);
+			console.log('cart', cart);
+			console.log('i', i);
+			// }
+		});
+	});
 });
