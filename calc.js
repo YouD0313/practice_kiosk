d37d4539115a8c32577f48dfbee3593c6563889d
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2022-11-16 13:42:33 +0900

    javascript css
    css btn
    javascript btn click event

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 let sum = 0;
 const spin = document.querySelectorAll('.spin');
 const button = document.querySelectorAll('.cartIcon');
+const sumText = document.querySelector('#sumtext');
 
 // button.addEventListener('click', function () {
 // 	sum += parseInt(button.value) * parseInt(spin.value);
@@ -11,7 +12,7 @@ const button = document.querySelectorAll('.cartIcon');
 button.forEach((cart, idx) => {
 	cart.addEventListener('click', () => {
 		sum += parseInt(button[idx].value) * parseInt(spin[idx].value);
-		document.getElementById('sumtext').value = sum + '원';
+		sumText.value = sum + '원';
 		console.log('button.value', button[idx].value);
 		console.log('spin.value', spin[idx].value);
 		spin[idx].value = 1;
@@ -20,4 +21,27 @@ button.forEach((cart, idx) => {
 		console.log('idx', idx);
 		// }
 	});
+});
+
+const charge = document.querySelector('.charge');
+charge.addEventListener('click', () => {
+	let fee = parseInt(prompt('금액을 투입하세요.', 10000));
+	if (fee) {
+		if (fee < sum) {
+			alert(Math.abs(fee - sum) + '원 부족합니다. 다시 금액을 투입하세요.');
+			parseInt(prompt('다시 금액을 투입하세요.', 10000));
+		} else {
+			alert('거스름돈은 ' + (fee - sum) + '원 입니다.');
+		}
+	} else {
+		alert('숫자를 입력하세요.');
+	}
+});
+const reject = document.querySelector('.reject');
+reject.addEventListener('click', () => {
+	if (confirm('정말 취소하겠습니까?')) {
+		alert('취소되었습니다.');
+		console.log('typeof', typeof sumText.value);
+		location.reload();
+	}
 });
