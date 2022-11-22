40323a4f4ac976186e1dd292d929b25c377e1285
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2022-11-22 21:11:25 +0900

    Update calc.js

--- calc.js
+++ calc.js
@@ -15,12 +15,12 @@ button.forEach((cart, idx) => {
 		// 	parseInt(this.value) * parseInt(this.parentElement.childNodes[3].value);
 		sum += parseInt(button[idx].value) * parseInt(spin[idx].value);
 		sumText.value = sum + '원';
-		console.log('button.value', button[idx].value);
-		console.log('spin.value', spin[idx].value);
+		// console.log('button.value', button[idx].value);
+		// console.log('spin.value', spin[idx].value);
 		spin[idx].value = 1;
-		console.log('sum', sum);
-		console.log('cart', cart);
-		console.log('idx', idx);
+		// console.log('sum', sum);
+		// console.log('cart', cart);
+		// console.log('idx', idx);
 	});
 });
 
@@ -30,9 +30,18 @@ function ifelse() {
 	console.log('fee', fee);
 	if (fee) {
 		if (fee < sum) {
-			for (let i = 1; i < 10; i++) {
-				alert(Math.abs(fee - sum) + '원 부족합니다.\n다시 금액을 투입하세요.');
-				let fixFee = parseInt(prompt('다시 금액을 투입하세요.', 10000));
+			console.log('outsidefor', fee);
+			for (let i = 1; i < 5; i++) {
+				console.log('insidefor', fee);
+				if (isNaN(fee) || fee == '') {
+					return;
+				} else {
+					console.log('insideforNelse', fee);
+					alert(
+						Math.abs(fee - sum) + '원 부족합니다.\n다시 금액을 투입하세요.'
+					);
+					let fixFee = parseInt(prompt('다시 금액을 투입하세요.', 10000));
+				}
 			}
 		} else {
 			alert('결제완료 되었습니다.\n거스름돈은 ' + (fee - sum) + '원 입니다.');
